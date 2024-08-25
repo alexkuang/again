ffb5967c255a4ccd0e20b267c4d162e004c183ae
Alex Kuang <alex@bikeshed.coffee>
2024-08-25 17:16:34 -0400

    more exec test

--- test/again/exec_test.exs
+++ test/again/exec_test.exs
@@ -36,7 +36,39 @@ defmodule Again.ExecTest do
       assert {:continue, backoff} == advanced.next
     end
 
-    test "errors when over-advancing" do
+    test "uses clock output when interacting with termination policy" do
+      backoff = 250
+      max_duration = 300
+
+      policy = %Policy{
+        clock: MockClock,
+        backoff: Backoff.constant_backoff(backoff),
+        termination: Termination.limit_duration(max_duration)
+      }
+
+      # Given a policy with 250ms backoff + max duration of 300ms, and t = 1 when we start execution
+      Mox.expect(MockClock, :tick, fn -> 1 end)
+      exec = Exec.init(policy)
+
+      # When we advance the first time @ t = 2
+      Mox.expect(MockClock, :tick, fn -> 2 end)
+      advanced = Exec.advance(exec)
+
+      # Then the advance succeeds because next attempt would be under threshold, i.e. (2ms tick + 250ms backoff) < 300
+      assert exec.attempts + 1 == advanced.attempts
+      assert backoff == advanced.last_backoff
+      assert {:continue, backoff} == advanced.next
+
+      # And when we advance the second time @ t=252
+      Mox.expect(MockClock, :tick, fn -> 252 end)
+      advanced = Exec.advance(exec)
+
+      # Then we halt because the next attempt would be over threshold, i.e. (252 + 250) > 300
+      assert exec.attempts == advanced.attempts
+      assert :halt == advanced.next
+    end
+
+    test "errors when advancing above configured attempt limit" do
       policy = %Policy{
         clock: MockClock,
         backoff: Backoff.constant_backoff(100),
